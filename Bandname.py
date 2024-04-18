29a4d243ea8301079d2a96f35f9527a8cffd88cf
Nerelwar <149014597+Nerelwar@users.noreply.github.com>
2024-04-18 23:18:53 +0530

    Bandname.py

---
+++ Bandname.py
@@ -0,0 +1,7 @@
+#Project Number 1
+#Creating a Band name
+print("Welcome to the Band Name Generator!")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What is your pet's name?\n")
+print("Your Band Name could be " + city +" " + pet)
+print("***** ***** ***** ****** ***** ***** ***** ***** ***** *****")
